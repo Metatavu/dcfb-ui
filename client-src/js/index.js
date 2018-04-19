d01fba4bccc7ee2d7782de0beb6c3c72a604271c
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-04-19 11:49:43 +0300

    Fixed bug which caused front page layout to break

--- client-src/js/index.js
+++ client-src/js/index.js
@@ -1,29 +1,30 @@
 (() => {
   "use strict";
   
-  $(document).ready(() => {
 
-    const iso = $( ".index-search-preview-container").isotope({
-      itemSelector: ".sale-poster",
-      layoutMode: "fitRows",
-      fitRows: {
-        gutter: 10
-      }
-    });
-    
-    $(".load-more-btn").click(() => {
-      $.getJSON("/search/ajax", (results) => {
-        const items = [];
-        results.forEach((result) => {
-          items.push($(pugSalePoster({item: result}))[0]);
-        });
-
-        iso
-          .append(items)
-          .isotope('appended', items);
-      });
-    });
-
+  const iso = $( ".index-search-preview-container").isotope({
+    itemSelector: ".sale-poster",
+    layoutMode: "fitRows",
+    fitRows: {
+      gutter: 10
+    }
   });
-  
+
+  iso.imagesLoaded().progress(() => {
+    iso.isotope('layout');
+  });
+
+  $(".load-more-btn").click(() => {
+    $.getJSON("/search/ajax", (results) => {
+      const items = [];
+      results.forEach((result) => {
+        items.push($(pugSalePoster({item: result}))[0]);
+      });
+
+      iso
+        .append(items)
+        .isotope('appended', items);
+    });
+  });
+
 })();
