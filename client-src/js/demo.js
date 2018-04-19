1809d1801787d009ac7bad0cc1c642ba360cc708
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-04-19 12:31:51 +0300

    Added demo version notifications

---
+++ client-src/js/demo.js
@@ -0,0 +1,12 @@
+(() => {
+  "use strict";
+  
+  //TODO: remove this while when the final version is implemented
+  
+  $(document).ready(() => {
+    $(".nav-item,.continue-btn,.buy-btn,.advanced-search-link").click(() => {
+      alert("This is only demo version of the service and not all of the functions are implemented.");
+    });    
+  });
+
+})();
